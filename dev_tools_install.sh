9f4b868bfbf7b49d1f3c6795917fe535d6a12c76
Dj140 <33819388+dj140@users.noreply.github.com>
2019-04-04 17:35:58 +0800

    Update dev_tools_install.sh

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -4,12 +4,12 @@ sudo apt-get update
 sudo apt-get install python-gudev
 sudo apt-get install -y avahi-daemon
 sudo apt-get install -y openssh-server
-sudo apt-get install -y arduino-core
+#sudo apt-get install -y arduino-core
 sudo easy_install pip
-sudo pip install -U platformio
-sudo rm -rf ~/.platformio/
+#sudo pip install -U platformio
+#sudo rm -rf ~/.platformio/
 #wget https://www.pjrc.com/teensy/49-teensy.rules
-sudo cp 49-teensy.rules /etc/udev/rules.d/
+#sudo cp 49-teensy.rules /etc/udev/rules.d/
 sudo cp 58-riki.rules /etc/udev/rules.d/
 sudo cp 558-orbbec-usb.rules /etc/udev/rules.d/
 
